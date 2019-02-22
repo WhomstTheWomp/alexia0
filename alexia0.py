53ba096bba249a1d11f651bad5edb0763c2ce96e
xAlexia0x <47811579+xAlexia0x@users.noreply.github.com>
2019-02-21 19:50:30 -0800

    Update alexia0.py

--- alexia0.py
+++ alexia0.py
@@ -113,18 +113,13 @@ async def on_message_delete(message):
     channel = message.channel
     author = message.author
     
-    await client.send_message(channel, 'Did you fuck up, @%s? (Message Deleted)' % author)
+    await client.send_message(channel, 'Did you fuck up, {}? (Message Deleted)' format(author))
 
     
 # Misc Commands
 @client.command()
 async def subgap():
     await client.say('https://www.youtube.com/watch?v=UVxU2HzPGug')
-    
-@client.command(pass_context=True)
-async def test(ctx):
-    author = ctx.message.author
-    await client.say('@{}'format(author))
     
     
 
